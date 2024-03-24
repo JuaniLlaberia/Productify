ac3f20967a0dbfe3d7f9ad1745a717ba5d2847f3
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-03-23 23:04:01 -0300

    Add tasks types and files to convex

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,6 +15,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as projects from "../projects.js";
+import type * as tasks from "../tasks.js";
 import type * as users from "../users.js";
 
 /**
@@ -27,6 +28,7 @@ import type * as users from "../users.js";
  */
 declare const fullApi: ApiFromModules<{
   projects: typeof projects;
+  tasks: typeof tasks;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
