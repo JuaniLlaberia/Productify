48012191e31a5cee9c67722fe6c530e047e4e16d
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-03-28 18:22:34 -0300

    Update clerk components style

--- src/components/ConvexClientProvider.tsx
+++ src/components/ConvexClientProvider.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider, useAuth } from '@clerk/nextjs';
 import { ConvexProviderWithClerk } from 'convex/react-clerk';
 import { ConvexReactClient } from 'convex/react';
 import { dark } from '@clerk/themes';
+import { useTheme } from 'next-themes';
 import type { ReactNode } from 'react';
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
@@ -13,13 +14,21 @@ export default function ConvexClientProvider({
 }: {
   children: ReactNode;
 }) {
+  const { theme } = useTheme();
+
   return (
     <ClerkProvider
       appearance={{
-        baseTheme: dark,
+        baseTheme: theme === 'light' ? undefined : dark,
+        elements: {
+          formButtonPrimary: 'bg-[#2b2929] hover:bg-[#1b1a1a]',
+          footerActionLink: 'text-[#ff9a27] underline hover:text-[#f3a64f]',
+        },
+        variables: { colorPrimary: '#2b2929' },
       }}
       afterSignInUrl='/projects'
       afterSignUpUrl='/projects'
+      signInUrl='/'
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
       <ConvexProviderWithClerk
