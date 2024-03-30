a425056e58b3aec69b77f9348d6e6ca00297e2b0
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-03-29 21:37:26 -0300

    Style clerk components

--- src/components/ConvexClientProvider.tsx
+++ src/components/ConvexClientProvider.tsx
@@ -21,8 +21,14 @@ export default function ConvexClientProvider({
       appearance={{
         baseTheme: theme === 'light' ? undefined : dark,
         elements: {
-          formButtonPrimary: 'bg-[#2b2929] hover:bg-[#1b1a1a]',
-          footerActionLink: 'text-[#ff9a27] underline hover:text-[#f3a64f]',
+          formButtonPrimary: 'bg-[#2b2929] hover:bg-[#272727] text-text-1',
+          footerActionLink: 'text-special underline hover:text-[#f3a64f]',
+          profileSectionPrimaryButton: 'text-text-2',
+          badge: 'text-special',
+          avatarImageActionsUpload: 'text-text-2',
+          formButtonReset: 'text-text-2',
+          fileDropAreaButtonPrimary: 'text-text-1',
+          scrollBox: 'rounded-lg',
         },
         variables: { colorPrimary: '#2b2929' },
       }}
@@ -31,10 +37,7 @@ export default function ConvexClientProvider({
       signInUrl='/'
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
-      <ConvexProviderWithClerk
-        client={convex}
-        useAuth={useAuth}
-      >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         {children}
       </ConvexProviderWithClerk>
     </ClerkProvider>
