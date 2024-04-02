7b575405a918fe77ef29e95d62b1daf70b162514
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-04-02 15:12:33 -0300

    Implement preloaded query for projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,15 @@
 import Link from 'next/link';
-import { fetchQuery } from 'convex/nextjs';
-import { HiOutlineFolder, HiOutlinePlus } from 'react-icons/hi2';
+import { preloadQuery } from 'convex/nextjs';
+import { HiOutlinePlus } from 'react-icons/hi2';
 
 import NavbarProjects from '@/app/projects/[projectId]/navbar-projects';
-import ProjectItem, { ExtendedDoc } from '@/app/projects/project-item';
+import ProjectsList from './project-list';
 import { api } from '../../../convex/_generated/api';
 import { getAuthToken } from '@/utils/getAuthToken';
 
 const ProjectsPage = async () => {
   const token = await getAuthToken();
-  const projects = await fetchQuery(
+  const preloadedProjects = await preloadQuery(
     api.projects.getUserProjects,
     {},
     { token }
@@ -19,9 +19,7 @@ const ProjectsPage = async () => {
     <main className='px-2'>
       <NavbarProjects />
       <header className='flex items-center justify-between p-2 px-3 lg:px-24 my-3'>
-        <h1 className='text-2xl text-text-1 font-semibold'>
-          Projects {projects.length > 0 ? `(${projects?.length})` : ''}
-        </h1>
+        <h1 className='text-2xl text-text-1 font-semibold'>Projects</h1>
         <Link
           href='/projects/new'
           className='flex items-center h-9 rounded-md px-4 hover:bg-background-hover-2 text-text-1'
@@ -30,19 +28,7 @@ const ProjectsPage = async () => {
           <span className='ml-3'>New project</span>
         </Link>
       </header>
-      <section className='lg:px-24'>
-        {projects.length > 0 ? (
-          <ul className='flex flex-col gap-2 w-full mt-3 lg:grid lg:grid-cols-3 lg:gap-4'>
-            {projects.map(project => (
-              <ProjectItem key={project._id} project={project as ExtendedDoc} />
-            ))}
-          </ul>
-        ) : (
-          <p className='flex items-center justify-center gap-3 text-sm text-text-2 my-16'>
-            <HiOutlineFolder size={20} /> No projects found
-          </p>
-        )}
-      </section>
+      <ProjectsList preloadedProjects={preloadedProjects} />
     </main>
   );
 };
---
+++ src/app/projects/project-list.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { Preloaded, usePreloadedQuery } from 'convex/react';
+import { HiOutlineFolder } from 'react-icons/hi2';
+
+import ProjectItem, { ExtendedDoc } from './project-item';
+import { api } from '../../../convex/_generated/api';
+
+const ProjectsList = (props: {
+  preloadedProjects: Preloaded<typeof api.projects.getUserProjects>;
+}) => {
+  const projects = usePreloadedQuery(props.preloadedProjects);
+
+  return (
+    <section className='lg:px-24'>
+      {projects.length > 0 ? (
+        <ul className='flex flex-col gap-2 w-full mt-3 lg:grid lg:grid-cols-3 lg:gap-4'>
+          {projects.map(project => (
+            <ProjectItem
+              key={project._id}
+              project={project as ExtendedDoc}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p className='flex items-center justify-center gap-3 text-sm text-text-2 my-16'>
+          <HiOutlineFolder size={20} /> No projects found
+        </p>
+      )}
+    </section>
+  );
+};
+
+export default ProjectsList;
