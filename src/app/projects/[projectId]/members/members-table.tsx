0fdaacfe0a1edbc26ec68d20f0ec2f10d980fc69
JuaniLlaberia <juanillaberia2002@gmail.com>
2024-04-01 21:02:41 -0300

    Update members page

--- src/app/projects/[projectId]/members/members-table.tsx
+++ src/app/projects/[projectId]/members/members-table.tsx
@@ -1,44 +1,19 @@
 'use client';
 
-import { usePaginatedQuery, useQuery } from 'convex/react';
-import {
-  HiOutlineArrowLeft,
-  HiOutlineArrowRight,
-  HiOutlineEllipsisVertical,
-  HiOutlineSparkles,
-  HiOutlineTrash,
-} from 'react-icons/hi2';
+import { usePaginatedQuery } from 'convex/react';
+import { HiOutlineArrowLeft, HiOutlineArrowRight } from 'react-icons/hi2';
 
-import Badge from '@/components/ui/badge';
+import MemberRow from './member-row';
 import { api } from '../../../../../convex/_generated/api';
 import { Id } from '../../../../../convex/_generated/dataModel';
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
-import {
-  AlertDialog,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
-import FormBtn from '@/components/form-btn';
 
 type MembersTableType = {
   projectId: Id<'projects'>;
@@ -48,14 +23,11 @@ const MembersTable = ({ projectId }: MembersTableType) => {
   const { results, status, loadMore } = usePaginatedQuery(
     api.projects.getMembersPaginated,
     { projectId },
-    { initialNumItems: 1 }
+    { initialNumItems: 5 }
   );
-
-  if (status === 'LoadingFirstPage') return <p>loading</p>;
 
   return (
     <>
-      <button onClick={() => loadMore(1)}>MORE</button>
       <Table className='border border-border-1'>
         <TableHeader>
           <TableRow>
@@ -68,84 +40,11 @@ const MembersTable = ({ projectId }: MembersTableType) => {
         </TableHeader>
         <TableBody>
           {results?.map(member => (
-            <TableRow>
-              <TableCell>
-                <Avatar>
-                  <AvatarFallback></AvatarFallback>
-                  <AvatarImage src={member.profileImg} />
-                </Avatar>
-              </TableCell>
-              <TableCell className='w-[150px] lg:w-[200px]'>
-                {member.name}
-              </TableCell>
-              <TableCell>{member.email}</TableCell>
-              <TableCell className='text-right'>
-                <div className='flex'>
-                  <Badge
-                    text={member.role as string}
-                    color={`${member.role === 'member' ? 'blue' : 'red'}`}
-                  />
-                </div>
-              </TableCell>
-              <TableCell className='flex justify-end mt-1'>
-                <AlertDialog>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Button
-                        variant='ghost'
-                        size='sm'
-                      >
-                        <HiOutlineEllipsisVertical size={20} />
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                      <DropdownMenuItem>
-                        <HiOutlineSparkles className='mr-2 h-4 w-4' />
-                        <span>Make admin</span>
-                      </DropdownMenuItem>
-                      <DropdownMenuItem
-                        className='text-text-danger'
-                        asChild
-                      >
-                        <AlertDialogTrigger className='w-full'>
-                          <>
-                            <HiOutlineTrash className='mr-2 h-4 w-4' />
-                            <span>Remove member</span>
-                          </>
-                        </AlertDialogTrigger>
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                  <AlertDialogContent>
-                    <AlertDialogTitle>Remove Member</AlertDialogTitle>
-                    <AlertDialogDescription className='text-text-2'>
-                      Are you sure you want to remove this member?{' '}
-                      <span className='text-text-1 font-semibold'>
-                        It can re-join the project again.
-                      </span>
-                    </AlertDialogDescription>
-                    <AlertDialogFooter className='flex flex-row justify-between'>
-                      <AlertDialogCancel asChild>
-                        <Button
-                          variant='ghost'
-                          size='sm'
-                        >
-                          Cancel
-                        </Button>
-                      </AlertDialogCancel>
-                      <form action={''}>
-                        <FormBtn
-                          dangerMode
-                          size='sm'
-                        >
-                          Confirm
-                        </FormBtn>
-                      </form>
-                    </AlertDialogFooter>
-                  </AlertDialogContent>
-                </AlertDialog>
-              </TableCell>
-            </TableRow>
+            <MemberRow
+              key={member._id}
+              member={member}
+              projectId={projectId}
+            />
           ))}
         </TableBody>
       </Table>
--- src/app/projects/[projectId]/members/page.tsx
+++ src/app/projects/[projectId]/members/page.tsx
@@ -1,20 +1,10 @@
-import { HiOutlineMagnifyingGlass } from 'react-icons/hi2';
-
 import MembersTable from './members-table';
 import { Id } from '../../../../../convex/_generated/dataModel';
-import { Input } from '@/components/ui/input';
 
 const MembersPage = ({ params }: { params: { projectId: string } }) => {
   return (
     <>
-      <header className='w-full flex flex-col md:flex-row lg:items-center lg:justify-between mb-4 lg:mb-6'>
-        <h1 className='text-xl w-full mb-3 lg:mb-0'>Members</h1>
-        <Input
-          placeholder='Search by email or name'
-          icon={<HiOutlineMagnifyingGlass />}
-          className='w-full md:min-w-[400px]'
-        />
-      </header>
+      <h1 className='text-xl w-full mb-4 lg:mb-8'>Members</h1>
       <MembersTable projectId={params.projectId as Id<'projects'>} />
     </>
   );
